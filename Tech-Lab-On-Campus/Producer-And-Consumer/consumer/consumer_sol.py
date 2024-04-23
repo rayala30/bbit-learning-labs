dd4ade057edd8ce0de6ee040dd2b75642adc5283
rayala30 <rafayala30@gmail.com>
2024-04-23 19:21:12 -0400

    consumer_sol progress

--- Tech-Lab-On-Campus/Producer-And-Consumer/consumer/consumer_sol.py
+++ Tech-Lab-On-Campus/Producer-And-Consumer/consumer/consumer_sol.py
@@ -1,1 +1,31 @@
 # Test File 
+# import mqConsumerInterface
+
+import os
+import pika
+
+from consumer_interface import mqConsumerInterface
+
+class mqConsumer(mqConsumerInterface):
+
+    def __init__(self):
+        mqConsumer = mqConsumer()
+
+        self._binding_key = ""
+        self._exchange_name = ""
+        self._queue_name = ""
+        
+
+    def setupRMQConnection(self):
+        pass
+
+    def onMessageCallback(self):
+        pass
+
+
+    def startConsuming(self):
+        pass
+
+
+
+
